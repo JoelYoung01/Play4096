afd6017c4d6ebac525931c262bac3695f28c3d13
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2025-08-29 16:36:50 -0500

    convert db to proxy to avoid build time errors

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -3,8 +3,21 @@ import Database from "better-sqlite3";
 import * as schema from "./schema";
 import { env } from "$env/dynamic/private";
 
-if (!env.DATABASE_URL) throw new Error("[DB] DATABASE_URL is not set");
+/** @type {any} */
+let dbInstance = null;
 
-const client = new Database(env.DATABASE_URL);
+function getDb() {
+	if (!dbInstance) {
+		if (!env.DATABASE_URL) throw new Error("[DB] DATABASE_URL is not set");
+		const client = new Database(env.DATABASE_URL);
+		dbInstance = drizzle(client, { schema });
+	}
+	return dbInstance;
+}
 
-export const db = drizzle(client, { schema });
+export { getDb };
+export const db = new Proxy({}, {
+	get(target, prop) {
+		return getDb()[prop];
+	}
+});
