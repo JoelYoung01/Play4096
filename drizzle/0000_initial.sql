4723e8c1f1e9dfa4c82c0dee751c3cf9504565f7
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2025-09-10 01:14:00 -0500

    Collapse migrations

--- drizzle/0000_initial.sql
+++ drizzle/0000_initial.sql
@@ -1,15 +1,34 @@
+CREATE TABLE `game` (
+	`id` text PRIMARY KEY NOT NULL,
+	`player_id` text NOT NULL,
+	`created_on` integer NOT NULL,
+	`completed_on` integer,
+	`score` integer,
+	`won` integer NOT NULL,
+	`complete` integer NOT NULL,
+	`board_json` text NOT NULL,
+	FOREIGN KEY (`player_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
 CREATE TABLE `session` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` text NOT NULL,
 	`expires_at` integer NOT NULL,
-	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE TABLE `user` (
 	`id` text PRIMARY KEY NOT NULL,
-	`age` integer,
 	`username` text NOT NULL,
 	`password_hash` text NOT NULL
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX `user_username_unique` ON `user` (`username`);
+CREATE UNIQUE INDEX `user_username_unique` ON `user` (`username`);--> statement-breakpoint
+CREATE TABLE `user_profile` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`display_name` text,
+	`avatar_url` text,
+	`best_score` integer,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
--- drizzle/0005_dashing_millenium_guard.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `user_profile` ADD `best_score` integer;
