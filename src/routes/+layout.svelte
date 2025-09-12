fc6f66c7e023c87ab719a9683b999a545c753f68
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2025-09-12 13:40:54 -0500

    Move scroll prevention to just /game route instead of app-wide

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -85,25 +85,5 @@
 		overflow: hidden;
 		position: relative;
 		height: 100dvh;
-
-		/* Prevent Chrome's pull-to-refresh and other touch gestures */
-		touch-action: none;
-		user-select: none;
-		overscroll-behavior: contain;
-		user-drag: none;
-		/* Additional gesture prevention */
-		-webkit-touch-callout: none;
-		-webkit-user-select: none;
-		-webkit-overflow-scrolling: touch;
-		-webkit-overscroll-behavior: contain;
-		-webkit-tap-highlight-color: transparent;
-		-webkit-touch-callout: none;
-		-webkit-user-drag: none;
-		-khtml-user-select: none;
-		-moz-user-select: none;
-		-ms-user-select: none;
-		-khtml-user-drag: none;
-		-moz-user-drag: none;
-		-o-user-drag: none;
 	}
 </style>
--- src/routes/game/+layout.svelte
+++ src/routes/game/+layout.svelte
@@ -10,4 +10,29 @@
 	});
 </script>
 
-{@render children?.()}
+<div class="game-container">
+	{@render children?.()}
+</div>
+
+<style>
+	.game-container {
+		/* Prevent Chrome's pull-to-refresh and other touch gestures */
+		touch-action: none;
+		user-select: none;
+		overscroll-behavior: contain;
+		/* Additional gesture prevention */
+		-webkit-touch-callout: none;
+		-webkit-user-select: none;
+		-webkit-overflow-scrolling: touch;
+		-webkit-overscroll-behavior: contain;
+		-webkit-tap-highlight-color: transparent;
+		-webkit-touch-callout: none;
+		-webkit-user-drag: none;
+		-khtml-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		-khtml-user-drag: none;
+		-moz-user-drag: none;
+		-o-user-drag: none;
+	}
+</style>
